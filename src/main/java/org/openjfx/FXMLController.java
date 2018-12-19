b98e95ba456b27d8be527965567673fe2456f34a
bch0ng <bchong@uw.edu>
2018-12-19 07:41:30 -0800

    [Update] Adds commments

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -3,24 +3,23 @@ package org.openjfx;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.net.URL;
-import java.util.EventObject;
 
 public class FXMLController {
+    // Size in pixels (px) for titlebar icons.
     private static final int TRAFFIC_LIGHT_ICONS_SIZE = 12;
 
+    // Stage given by MainApp.java
     private Stage stage;
 
+    // CSS ID's used in scene.fxml
     @FXML
     private HBox titlebar;
     @FXML
@@ -38,25 +37,38 @@ public class FXMLController {
 
     public void initialize() {
         this.createTitlebar();
-
     }
 
+    /**
+     * Creates a custom titlebar with custom decorations
+     * (the close, minimize, and resize buttons)
+     * imitating the MacOS traffic light icons.
+     */
     private void createTitlebar() {
+        /*
+        // Creates image containers with default icons,
+        // then sets their width and height, and then
+        // adds the image to their respective button.
+        */
         ImageView closeImageView = new ImageView(getClass()
                 .getResource("images/titlebar_icons/small/small_red.png").toExternalForm());
         closeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
         closeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
         closeButton.setGraphic(closeImageView);
+
         ImageView minimizeImageView = new ImageView(getClass()
                 .getResource("images/titlebar_icons/small/small_yellow.png").toExternalForm());
         minimizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
         minimizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
         minimizeButton.setGraphic(minimizeImageView);
+
         ImageView resizeImageView = new ImageView(getClass()
                 .getResource("images/titlebar_icons/small/small_green.png").toExternalForm());
         resizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
         resizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
         resizeButton.setGraphic(resizeImageView);
+
+        // On mouse hover enter on decorations, show descriptive icons
         titlebar.setOnMouseEntered(new EventHandler<>() {
             @Override
             public void handle(MouseEvent t)
@@ -75,6 +87,7 @@ public class FXMLController {
                 resizeButton.setGraphic(resizeImageView);
             }
         });
+        // On mouse hover exit on decorations, show default icons
         titlebar.setOnMouseExited(new EventHandler<>() {
             @Override
             public void handle(MouseEvent t)
@@ -99,12 +112,18 @@ public class FXMLController {
                         .toExternalForm())));
     }
 
+    /**
+     * Changes decorations to light-gray when window is
+     * not focused, and back to default colors when focused.
+     */
     public void handleUnfocusedStage() {
+        // Image containers that are put inside of Buttons
         ImageView closeImageView = new ImageView();
         ImageView minimizeImageView = new ImageView();
         ImageView resizeImageView = new ImageView();
         stage.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
             if (!isNowFocused) {
+                // Changing all three decorations to light gray
                 closeImageView.setImage(new Image(getClass()
                         .getResource("images/titlebar_icons/small/small_unfocused.png")
                         .toExternalForm()));
@@ -118,6 +137,7 @@ public class FXMLController {
                         .toExternalForm()));
                 resizeButton.setGraphic(resizeImageView);
             } else {
+                // Reverting all three decorations back to default
                 setDefaultTitlebarIcons(closeImageView, minimizeImageView, resizeImageView, getClass()
                         .getResource("images/titlebar_icons/small/small_red.png"), getClass()
                         .getResource("images/titlebar_icons/small/small_yellow.png"), getClass()
@@ -126,27 +146,55 @@ public class FXMLController {
         });
     }
 
-    private void setDefaultTitlebarIcons(ImageView closeImageView, ImageView minimizeImageView, ImageView resizeImageView, URL resource, URL resource2, URL resource3) {
-        closeImageView.setImage(new Image(resource
+    /** HELPER FUNCTION
+     * Sets decorations to their default traffic light colors.
+     *
+     * @param closeImageView        image container for the close decoration
+     * @param minimizeImageView     image container for the minimize decoration
+     * @param resizeImageView       image container for the resize decoration
+     * @param closeDecoURL          URL to default close decoration icon
+     * @param minimizeDecoURL       URL to default minimize decoration icon
+     * @param resizeDecoURL         URL to default resize decoration icon
+     */
+    private void setDefaultTitlebarIcons(ImageView closeImageView, ImageView minimizeImageView,
+            ImageView resizeImageView, URL closeDecoURL, URL minimizeDecoURL, URL resizeDecoURL)
+    {
+        closeImageView.setImage(new Image(closeDecoURL
                 .toExternalForm()));
         closeButton.setGraphic(closeImageView);
-        minimizeImageView.setImage(new Image(resource2
+        minimizeImageView.setImage(new Image(minimizeDecoURL
                 .toExternalForm()));
         minimizeButton.setGraphic(minimizeImageView);
-        resizeImageView.setImage(new Image(resource3
+        resizeImageView.setImage(new Image(resizeDecoURL
                 .toExternalForm()));
         resizeButton.setGraphic(resizeImageView);
     }
 
+    /**
+     * Closes window when close decoration is pressed.
+     *
+     * @param event mouse press by the user
+     */
     public void handleCloseButtonAction(ActionEvent event) {
         stage.close();
     }
 
+    /**
+     * Minimizes window when close decoration is pressed.
+     *
+     * @param event mouse press by the user
+     */
     public void handleMinimizeButtonAction(ActionEvent event) {
         stage.toBack();
         stage.setIconified(true);
     }
 
+    /**
+     * Creates a reference to the stage
+     * from MainApp.java
+     *
+     * @param stage application window
+     */
     public void setStage(Stage stage){
         this.stage = stage;
     }
--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -1,34 +1,26 @@
 package org.openjfx;
 
-import client.Client;
-import client.XMPPConnection;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.ToolBar;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
-    private double xOffset = 0;
-    private double yOffset = 0;
+    private double xOffset = 0; // x-location of window
+    private double yOffset = 0; // y-location of window
 
     @Override
     public void start(Stage stage) throws Exception
     {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("scene.fxml"));
         Parent root = loader.load();
+        // Moves window when pressed and dragged around
         root.setOnMousePressed((MouseEvent event) -> {
-            System.out.println("lol");
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
         });
@@ -37,16 +29,16 @@ public class MainApp extends Application {
             stage.setY(event.getScreenY() - yOffset);
         });
         Scene scene = new Scene(root, 387, 660);
+        // Sends stage to FXMLController.java and initializes
+        // handling when the window unfocuses.
         ((FXMLController) loader.getController()).setStage(stage);
         ((FXMLController) loader.getController()).handleUnfocusedStage();
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        // Required to round the window borders
         scene.setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
         stage.show();
-
-        ToolBar toolBar = new ToolBar();
     }
 
     public static void main(String[] args) throws Exception
@@ -54,6 +46,8 @@ public class MainApp extends Application {
         // For testing purposes
         //Client c = new Client();
         //c.main(args);
+
+        // Connecting GUI to client package.
         //XMPPConnection x = new XMPPConnection("test1", "testing1234");
         //System.out.println("Success!");
         Application.launch(args);
