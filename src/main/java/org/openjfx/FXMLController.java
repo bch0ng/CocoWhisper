4d02f76e7df77d75f848b26fc395a1118c77c182
bch0ng <bchong@uw.edu>
2018-12-20 16:11:11 -0800

    [Fix] Moves titlebar icons from controller to CSS

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -3,23 +3,15 @@ package org.openjfx;
 import client.ChattyXMPPConnection;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.jivesoftware.smack.roster.RosterEntry;
-
-import java.net.URL;
 import java.util.Collection;
 
 public class FXMLController {
@@ -27,15 +19,6 @@ public class FXMLController {
     private static final int TRAFFIC_LIGHT_ICONS_SIZE = 12;
 
     // Decoration image paths
-    private static final String CLOSE_DECO_DEFAULT_PATH = "images/titlebar_icons/medium/red.png";
-    private static final String CLOSE_DECO_HOVER_PATH = "images/titlebar_icons/medium/red_hover.png";
-    private static final String CLOSE_DECO_ACTIVE_PATH = "images/titlebar_icons/medium/red_active.png";
-    private static final String MIN_DECO_DEFAULT_PATH = "images/titlebar_icons/medium/yellow.png";
-    private static final String MIN_DECO_HOVER_PATH = "images/titlebar_icons/medium/yellow_hover.png";
-    private static final String MIN_DECO_ACTIVE_PATH = "images/titlebar_icons/medium/yellow_active.png";
-    private static final String RESIZE_DECO_DEFAULT_PATH = "images/titlebar_icons/medium/green.png";
-    private static final String RESIZE_DECO_HOVER_PATH = "images/titlebar_icons/medium/green_hover.png";
-    private static final String RESIZE_DECO_ACTIVE_PATH = "images/titlebar_icons/medium/green_active.png";
     private static final String UNFOCUSED_DECO_PATH = "images/titlebar_icons/medium/unfocused.png";
 
     private Stage stage;            // Stage given by MainApp.java
@@ -68,87 +51,6 @@ public class FXMLController {
         } catch (Exception e) {
             System.out.println("oh no");
         }
-        this.createTitlebar();
-    }
-
-    /**
-     * Creates a custom titlebar with custom decorations
-     * (the close, minimize, and resize buttons)
-     * imitating the MacOS traffic light icons.
-     */
-    private void createTitlebar() {
-        /*
-        // Creates image containers with default icons,
-        // then sets their width and height, and then
-        // adds the image to their respective button.
-        */
-        ImageView closeImageView = new ImageView(getClass()
-                .getResource(CLOSE_DECO_DEFAULT_PATH).toExternalForm());
-        closeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        closeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        closeButton.setGraphic(closeImageView);
-
-        ImageView minimizeImageView = new ImageView(getClass()
-                .getResource(MIN_DECO_DEFAULT_PATH).toExternalForm());
-        minimizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        minimizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        minimizeButton.setGraphic(minimizeImageView);
-
-        ImageView resizeImageView = new ImageView(getClass()
-                .getResource(RESIZE_DECO_DEFAULT_PATH).toExternalForm());
-        resizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        resizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        resizeButton.setGraphic(resizeImageView);
-
-        // On mouse hover enter on decorations, show descriptive icons
-        titlebar.setOnMouseEntered(new EventHandler<>() {
-            @Override
-            public void handle(MouseEvent t)
-            {
-                closeImageView.setImage(new Image(getClass()
-                        .getResource(CLOSE_DECO_HOVER_PATH)
-                        .toExternalForm()));
-                closeButton.setGraphic(closeImageView);
-                minimizeImageView.setImage(new Image(getClass()
-                        .getResource(MIN_DECO_HOVER_PATH)
-                        .toExternalForm()));
-                minimizeButton.setGraphic(minimizeImageView);
-                resizeImageView.setImage(new Image(getClass()
-                        .getResource(RESIZE_DECO_HOVER_PATH)
-                        .toExternalForm()));
-                resizeButton.setGraphic(resizeImageView);
-            }
-        });
-        // On mouse hover exit on decorations, show default icons
-        titlebar.setOnMouseExited(new EventHandler<>() {
-            @Override
-            public void handle(MouseEvent t)
-            {
-                if (isFocused) {
-                    setDefaultTitlebarIcons(closeImageView, minimizeImageView, resizeImageView, getClass()
-                            .getResource(CLOSE_DECO_DEFAULT_PATH), getClass()
-                            .getResource(MIN_DECO_DEFAULT_PATH), getClass()
-                            .getResource(RESIZE_DECO_DEFAULT_PATH));
-                } else {
-                    setDefaultTitlebarIcons(closeImageView, minimizeImageView, resizeImageView, getClass()
-                            .getResource(UNFOCUSED_DECO_PATH), getClass()
-                            .getResource(UNFOCUSED_DECO_PATH), getClass()
-                            .getResource(UNFOCUSED_DECO_PATH));
-                }
-            }
-        });
-        closeButton.setOnMousePressed((MouseEvent e) ->
-                closeImageView.setImage(new Image(getClass()
-                        .getResource(CLOSE_DECO_ACTIVE_PATH)
-                        .toExternalForm())));
-        minimizeButton.setOnMousePressed((MouseEvent e) ->
-                minimizeImageView.setImage(new Image(getClass()
-                        .getResource(MIN_DECO_ACTIVE_PATH)
-                        .toExternalForm())));
-        resizeButton.setOnMousePressed((MouseEvent e) ->
-                resizeImageView.setImage(new Image(getClass()
-                        .getResource(RESIZE_DECO_ACTIVE_PATH)
-                        .toExternalForm())));
     }
 
     /**
@@ -156,64 +58,20 @@ public class FXMLController {
      * not focused, and back to default colors when focused.
      */
     public void handleUnfocusedStage() {
-        // Image containers that are put inside of Buttons
-        ImageView closeImageView = new ImageView();
-        ImageView minimizeImageView = new ImageView();
-        ImageView resizeImageView = new ImageView();
         this.stage.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
             this.isFocused = isNowFocused;
             if (!isNowFocused) {
                 // Changing all three decorations to light gray
-                closeImageView.setImage(new Image(getClass()
-                        .getResource(UNFOCUSED_DECO_PATH)
-                        .toExternalForm()));
-                closeButton.setGraphic(closeImageView);
-                minimizeImageView.setImage(new Image(getClass()
-                        .getResource(UNFOCUSED_DECO_PATH)
-                        .toExternalForm()));
-                minimizeButton.setGraphic(minimizeImageView);
-                resizeImageView.setImage(new Image(getClass()
-                        .getResource(UNFOCUSED_DECO_PATH)
-                        .toExternalForm()));
-                resizeButton.setGraphic(resizeImageView);
+                closeButton.getStyleClass().add("unfocused-button");
+                minimizeButton.getStyleClass().add("unfocused-button");
+                resizeButton.getStyleClass().add("unfocused-button");
             } else {
                 // Reverting all three decorations back to default
-                setDefaultTitlebarIcons(closeImageView, minimizeImageView, resizeImageView, getClass()
-                        .getResource(CLOSE_DECO_DEFAULT_PATH), getClass()
-                        .getResource(MIN_DECO_DEFAULT_PATH), getClass()
-                        .getResource(RESIZE_DECO_DEFAULT_PATH));
+                closeButton.getStyleClass().remove("unfocused-button");
+                minimizeButton.getStyleClass().remove("unfocused-button");
+                resizeButton.getStyleClass().remove("unfocused-button");
             }
         });
-    }
-
-    /** HELPER FUNCTION
-     * Sets decorations to their default traffic light colors.
-     *
-     * @param closeImageView        image container for the close decoration
-     * @param minimizeImageView     image container for the minimize decoration
-     * @param resizeImageView       image container for the resize decoration
-     * @param closeDecoPath          URL to default close decoration icon
-     * @param minimizeDecoPath       URL to default minimize decoration icon
-     * @param resizeDecoPath         URL to default resize decoration icon
-     */
-    private void setDefaultTitlebarIcons(ImageView closeImageView, ImageView minimizeImageView,
-            ImageView resizeImageView, URL closeDecoPath, URL minimizeDecoPath, URL resizeDecoPath)
-    {
-        closeImageView.setImage(new Image(closeDecoPath
-                .toExternalForm()));
-        closeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        closeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        closeButton.setGraphic(closeImageView);
-        minimizeImageView.setImage(new Image(minimizeDecoPath
-                .toExternalForm()));
-        minimizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        minimizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        minimizeButton.setGraphic(minimizeImageView);
-        resizeImageView.setImage(new Image(resizeDecoPath
-                .toExternalForm()));
-        resizeImageView.setFitWidth(TRAFFIC_LIGHT_ICONS_SIZE);
-        resizeImageView.setFitHeight(TRAFFIC_LIGHT_ICONS_SIZE);
-        resizeButton.setGraphic(resizeImageView);
     }
 
     /**
